2835e8b6f060ee904152d3eae86dc1f42d9d25a1
Filip Todoroski <ftodoroski@ymail.com>
2019-11-16 14:36:18 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,11 +8,13 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   array_of_hashes = [
-    { name:  "Don Gately", occupation: "Live-in Staff" },
-    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-    { name:  "Pat Monteseian", occupation: "Staff" },
-    { name:  "Kate Gompert", occupation: "None" },
-    { name:  "Bruce Green", occupation: "Fan of Mildred" }
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+
+
+
   ]
 end
 
